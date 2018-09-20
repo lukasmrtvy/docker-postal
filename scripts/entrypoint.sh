7dd5ad766b23c4e9d49d35b48a74c63f3876ddad
Lukas M <lukas.mrtvy@gmail.com>
2018-09-20 16:57:37 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ ${DB_MIGRATE,,} == true];then
   mysql -h ${main_db.host} -P 3306 -uroot -p mypass -Bse 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "XXX"'
+  /opt/postal/bin/postal initialize
 fi
 
 if [ "${USER_CREATE,,}" == true]; then
