d4e7b4b6263a003246f2fbcaf5cd3c77a0f119a0
Lukas M <lukas.mrtvy@gmail.com>
2018-09-22 18:38:09 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+eval "cat <<EOF
+$(</config/config.template.yaml)
+EOF
+" | tee /config/config.yaml 2> /dev/null
+
+
+
 if [ ${DB_MIGRATE,,} == true];then
   mysql -h ${main_db.host} -P 3306 -uroot -p mypass -Bse 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "XXX"'
   /opt/postal/bin/postal initialize
