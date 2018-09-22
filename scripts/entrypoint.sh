8073c5b350ee1163c1c6a8e098faae06c3b297a1
Lukas M <lukas.mrtvy@gmail.com>
2018-09-22 19:53:56 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-if [ ${INIT_CONFIG,,} == true ];then
+export SD_USER="${SD_USER:-admin}"
+export SD_PASS="${SD_PASS:-admin}"
+
+if [ "${INIT_CONFIG,,}" == true ];then
   eval "cat <<EOF
     $(</opt/postal/config/postal.template.yaml)
 EOF
   " | tee /opt/postal/config/postal.yaml 2> /dev/null
 fi
 
-if [ ${DB_MIGRATE,,} == true];then
+if [ "${DB_MIGRATE,,}" == true ];then
   mysql -h ${main_db.host} -P 3306 -uroot -p mypass -Bse 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "XXX"'
   /opt/postal/bin/postal initialize
 fi
 
-if [ "${USER_CREATE,,}" == true]; then
+if [ "${USER_CREATE,,}" == true ]; then
    /opt/postal/bin/postal make-user
 fi 
 
 exec "$@"
-
