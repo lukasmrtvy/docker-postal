16da696af8ab2d6798b03edd6308048403fc81cd
Lukas M <lukas.mrtvy@gmail.com>
2018-09-23 17:23:17 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,7 +11,7 @@ EOF
 fi
 
 if [ "${DB_MIGRATE,,}" == true ];then
-  mysql -h ${main_db.host} -P 3306 -uroot -p mypass -Bse 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "XXX"'
+  #mysql -h ${main_db.host} -P 3306 -uroot -p mypass -Bse 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "XXX"'
   /opt/postal/bin/postal initialize
 fi
 
