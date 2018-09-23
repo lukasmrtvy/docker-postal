e5d9d8f55dc524f9a14589324ffcb1f9b2e27716
Lukas M <lukas.mrtvy@gmail.com>
2018-09-23 18:14:49 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ export SD_USER="${SD_USER:-admin}"
 export SD_PASS="${SD_PASS:-admin}"
 
 if [ "${INIT_CONFIG,,}" == true ];then
-  eval "cat <<EOF
+  eval "cat <<-EOF
     $(</opt/postal/config/postal.template.yml)
 EOF
   " | tee /opt/postal/config/postal.yml 2> /dev/null
